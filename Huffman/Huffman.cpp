9c51c1fbe0fa7ed4e534df39111ab482df2e8b2b
国超 夏 <istillalive@live.cn>
2016-12-01 13:47:36 +0800

    change c to cpp

--- Huffman/Huffman.cpp
+++ Huffman/Huffman.cpp
@@ -1,3 +1,4 @@
+/*
 #include<stdio.h>
 #include<malloc.h>
 
@@ -13,14 +14,33 @@ void input(int *w, char *l, int n)
 {
 	int i;
 	printf("Please enter letters:");
-	for(i = 0; i < n; i++0)
+	for(i = 0; i < n; i++ )
 		scanf("%c",&l[i]);
 	printf("Please enter letter's weight:");
-	for(i = 0; i < n; i++0)
+	for(i = 0; i < n; i++ )
 		scanf("%d",&w[i]);
 }
 
 int main()
 {
 	int n;
-	int *w,*l;
+	int *w, *l;
+}*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+
+//--------huffman tree的编码存储表示---------
+typedef struct {
+	unsigned int weight;
+	unsigned int parent, lchild, rchild;
+}HTNode, *HuffmanTree;               //动态分配数组存储huffman tree
+
+typedef char * *HuffmanCode;		//动态分配数组存储huffman 编码表
+
+void HuffmanCoding(HuffmanTree &HT, HuffmanCode &HC, int * w, int n) {
+	if (n <= 1)
+		return;
+
+}
